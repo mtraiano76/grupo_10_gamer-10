854fa6f2db1cdd9826e4c940bc2998d1f2a67f15
juanphenaor <96088656+juanphenaor@users.noreply.github.com>
2022-01-23 19:36:02 -0500

    Correcciones carrito de compras

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './view/index.html'));
 })
 
-app.get('/productCart', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './view/productCart.html'));
-})
 
 app.get('/login', function(req, res){
     res.sendFile(path.resolve(__dirname, './view/login.html'));
