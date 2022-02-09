582619dcecec0ea4c445355d9dd3826b4a4e9673
juanphenaor <96088656+juanphenaor@users.noreply.github.com>
2022-02-09 09:27:19 -0500

    Se agrega un select en los formularios de producto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -7,105 +7,119 @@ let products = [
        'url':'../images/Imágenes de Juegos PS4/Assassin’s Creed Valhalla.jpg',
        'name':'Assassin’s Creed Valhalla',
        'price':20000,
+       'category':'Acción'
    },
    {
        'id':2,
        'url':'../images/Imágenes de Juegos PS4/Call of Duty Black Ops Cold War.jpg',
        'name':'Black Ops Cold War',
        'price':30000,
+       'category':'Guerra'
    },
    {
        'id':3,
        'url':'../images/Imágenes de Juegos PS4/Dragon Ball Fighterz.jpg',
        'name':'Dragon Ball Fighterz',
        'price':40000,
+       'category':'Pelea'
    },
    {
        'id':4,
        'url':'../images/Imágenes de Juegos PS4/FIFA-22.jpg',
        'name':'FIFA-22',
        'price':50000,
+       'category':'Deporte'
    },
    {
        'id':5,
        'url':'../images/Imágenes de Juegos PS4/God of War III Remastered.jpg',
        'name':'God of War III Remastered',
        'price':50000,
+       'category':'Acción'
    },
    {
        'id':6,
        'url':'../images/Imágenes de Juegos PS4/Jump Force.jpg',
        'name':'Jump Force',
        'price':40000,
+       'category':'Pelea'
    },
    {
        'id':7,
        'url':'../images/Imágenes de Juegos PS4/LEGO Marvel Super Heroes 2.jpg',
        'name':'LEGO Marvel Super Heroes 2',
        'price':30000,
+       'category':'Acción'
    },
    {
        'id':8,
        'url':'../images/Imágenes de Juegos PS4/Metal gear solid V the phantom pain.jpg',
        'name':'Metal gear solid V the phantom pain',
        'price':20000,
+       'category':'Role'
    },
    {
        'id':9,
        'url':'../images/Imágenes de Juegos PS4/mortal kombat xl.jpg',
        'name':'mortal kombat xl',
        'price':60000,
+       'category':'Pelea'
    },
    {
        'id':10,
        'url':'../images/Imágenes de Juegos PS4/Need for Speed Heat.jpg',
        'name':'Need for Speed Heat',
        'price':80000,
+       'category':'Carreras'
    },
 ]
 
 let cartProducts = [
-   {
-      'id':1,
-      'url':'../images/Imágenes de Juegos PS4/Assassin’s Creed Valhalla.jpg',
-      'name':'Assassin’s Creed Valhalla',
-      'price':20000,
-      'quantity':2
-  },
-  {
-      'id':2,
-      'url':'../images/Imágenes de Juegos PS4/Call of Duty Black Ops Cold War.jpg',
-      'name':'Black Ops Cold War',
-      'price':30000,
-      'quantity':1
-  },
+    {
+        'id':1,
+        'url':'../images/Imágenes de Juegos PS4/Assassin’s Creed Valhalla.jpg',
+        'name':'Assassin’s Creed Valhalla',
+        'price':20000,
+        'category':'Acción'
+    },
+    {
+        'id':2,
+        'url':'../images/Imágenes de Juegos PS4/Call of Duty Black Ops Cold War.jpg',
+        'name':'Black Ops Cold War',
+        'price':30000,
+        'category':'Guerra'
+    },
 ]
 
 let suggestedProducts = [
-   {
-      'id':5,
-      'url':'../images/Imágenes de Juegos PS4/God of War III Remastered.jpg',
-      'name':'God of War III Remastered',
-      'price':50000,
-  },
-  {
-      'id':6,
-      'url':'../images/Imágenes de Juegos PS4/Jump Force.jpg',
-      'name':'Jump Force',
-      'price':40000,
-  },
-  {
-      'id':7,
-      'url':'../images/Imágenes de Juegos PS4/LEGO Marvel Super Heroes 2.jpg',
-      'name':'LEGO Marvel Super Heroes 2',
-      'price':30000,
-  },
-  {
-      'id':8,
-      'url':'../images/Imágenes de Juegos PS4/Metal gear solid V the phantom pain.jpg',
-      'name':'Metal gear solid V the phantom pain',
-      'price': 20000,
-  },
+    {
+        'id':5,
+        'url':'../images/Imágenes de Juegos PS4/God of War III Remastered.jpg',
+        'name':'God of War III Remastered',
+        'price':50000,
+        'category':'Acción'
+    },
+    {
+        'id':6,
+        'url':'../images/Imágenes de Juegos PS4/Jump Force.jpg',
+        'name':'Jump Force',
+        'price':40000,
+        'category':'Pelea'
+    },
+    {
+        'id':7,
+        'url':'../images/Imágenes de Juegos PS4/LEGO Marvel Super Heroes 2.jpg',
+        'name':'LEGO Marvel Super Heroes 2',
+        'price':30000,
+        'category':'Acción'
+    },
+    {
+        'id':8,
+        'url':'../images/Imágenes de Juegos PS4/Metal gear solid V the phantom pain.jpg',
+        'name':'Metal gear solid V the phantom pain',
+        'price':20000,
+        'category':'Role'
+    },
 ]
 
 let productsController = {
@@ -144,6 +158,7 @@ let productsController = {
             'url':request.url,
             'name':request.name,
             'price': request.price,
+            'category':request.category
         }
         products.push(newProduct);
         res.redirect("/products")
@@ -166,6 +181,7 @@ let productsController = {
 
         product.price = request.price;
         product.url = request.url;
+        product.category = request.category
         
         res.redirect("/products");
     },
