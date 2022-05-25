0b3fa63321a020d466fd85bf90c06c7994883116
juanphenaor <96088656+juanphenaor@users.noreply.github.com>
2022-05-24 20:08:22 -0500

    Corrección relación en payment

--- database/models/payment.js
+++ database/models/payment.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Payer.belongsTo(models.ShoppingCart, {
+      Payment.belongsTo(models.ShoppingCart, {
         as: 'shoppingCart',
         foreignKey: 'shoppingCartId'
       });
